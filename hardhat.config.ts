a0897e18221d9b6ff3101ac10654958b8993e875
AlSirang <uddinbashir1999@gmail.com>
2022-07-11 02:28:39 +0500

    fix: update the env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
 
 const config: HardhatUserConfig = {
-  solidity: "^0.8.0",
+  solidity: "0.8.7",
+
+  paths: { tests: "tests" },
 };
 
 export default config;
